46d40522a4682cef305db39c15bc73a10904c23c
eesch001 <eesch001@ucr.edu>
2020-07-21 18:22:16 -0700

    add all files

---
+++ index.js
@@ -0,0 +1,35 @@
+function openSlideMenu(){
+  document.getElementById('side-menu').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+}
+
+function closeSlideMenu(){
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
+}
+
+
+function openSlideMenu2(){
+  document.getElementById('side-menu2').style.width = '250px';
+  document.getElementById('main2').style.marginRight = '250px';
+}
+
+function closeSlideMenu2(){
+  document.getElementById('side-menu2').style.width = '0';
+  document.getElementById('main2').style.marginRight = '0';
+}
+
+
+
+// var searchbar=document.querySelector("input");
+// var searchinput=""
+//
+// searchbar.addEventListener("keydown", runEvent);
+// searchbar.addEventListener("keyup", runEvent);
+//
+// function runEvent(searchinput){
+//
+//
+//
+//   window.location.href = "https://losangeles.craigslist.org/search/sss?query="+value+ "&sort=rel";
+// }
